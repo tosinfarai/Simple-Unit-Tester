41ce098d733959c5125517462683f3d46fc5e4cc
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-04-01 19:35:59 +0100

    Add files via upload

---
+++ SimpleUnitTester.java
@@ -0,0 +1,32 @@
+package com.semanticsquare.jvm;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class SimpleUnitTester {
+
+	public static int execute(Class clazz) throws Exception {
+		int failedCount = 0;
+
+		//clazz = Reflection.class;
+		
+		 Constructor ReflectionCon = clazz.getDeclaredConstructor(); 
+		 Reflection obj = (Reflection) ReflectionCon.newInstance();
+
+		for (Method m : clazz.getDeclaredMethods()) {
+			if ((m.getReturnType() == boolean.class) && (m.getName().startsWith("test"))) {
+				Object returnedValue = m.invoke();
+				if ((boolean) (returnedValue = false) ) {
+					failedCount++;
+				}
+				
+			}
+		}
+		System.out.println(failedCount);
+		return failedCount;
+	}
+
+	public static void main(String[] args) throws Exception {
+		execute(Reflection.class);
+	}
+}
